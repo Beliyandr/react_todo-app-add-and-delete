6c366526b04dccd66802a560ce4ae4805295bffb
beliyandr <beliyandr@gmail.com>
2025-03-12 23:27:59 +0200

    432

--- src/App.tsx
+++ src/App.tsx
@@ -279,7 +279,10 @@ export const App: React.FC = () => {
 
                   {/* 'is-active' class puts this modal on top of the todo */}
                   <div data-cy="TodoLoader" className="modal overlay is-active">
-                    <div className="modal-background has-background-white-ter" />
+                    <div
+                      className="modal-background
+                     has-background-white-ter"
+                    />
                     <div className="loader" />
                   </div>
                 </div>
